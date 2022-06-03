6ab7b8ac583ad0c99b7966b5a7f2f6616ffda985
Yan Pritzker <yan@Yans-MacBook-Pro.local>
2022-06-03 15:57:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Try this app
+
+Generate your keys
+
+    openssl genrsa -out private.pem 4096
+    openssl rsa -in private.pem -pubout -outform PEM -out public.pub
+
+Visit your Swan Dashboard and register a new app. Copy the App ID.
+
+Run this app
+
+    AMOUNT_USD=100 APP_ID=[app id from above] yarn start
+
+The app will pick up the private.pem key from this directory and use the APP_ID from the supplied command line to make a buy on your behalf.
+
+WARNING: if you run this against the production API, it will actually make a
+buy on your account (if you have a dollar balance)! In order to change to production
+you will have to modify the URL string in the code to point to api.swanbitcoin.com.
---
+++ index.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const fs = require('fs');
+
+const appId = process.env.APP_ID;
+const purchaseAmountUsd = process.env.AMOUNT_USD;
+
+(async() => {
+  // Example payload to make a trading (buy Bitcoin) request
+  const payload = {
+    iss: appId,
+    scopes: ["write:trades"]
+  }
+
+  // In this demo, we're reading the private key from the file system
+  // and storing it in git.  DO NOT DO THIS in production. Read about securing your key here:
+  // https://developers.swanbitcoin.com/docs/personal-access/authentication#securing-private-keys
+  const privateKey = fs.readFileSync("private.pem")
+
+  const token = jwt.sign(payload, privateKey, {algorithm: 'RS256', expiresIn: '5s'});
+
+  // make your API request
+  const url = 'https://dev-api.swanbitcoin.com/integrations/v20220222/trades/execute/market';
+  
+  let response;
+
+  try {
+    response = await axios.post(url, 
+      { 
+        fundsType: 'balance', 
+        purchaseAmountUsd
+      }, 
+      { headers: {'Authorization': `Bearer ${token}`}}
+    );
+
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
+
+})();
