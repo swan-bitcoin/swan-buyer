f4fb16de7462fe331e916588b1ef3d2b2949d575
Yan Pritzker <yan@Yans-MacBook-Pro.local>
2022-06-05 10:04:17 -0400

    Cleanup

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const uuid = require('uuid').v4;
 const appId = process.env.APP_ID;
 const purchaseAmountUsd = process.env.AMOUNT_USD;
 
-(async() => {
+const createApiToken = ({ scopes }) => {
   // Example payload to make a trading (buy Bitcoin) request
   const payload = {
     // iss: Who you claim to be. Our server will validate this using the public key you uploaded, and the private key you used to sign this message below
     iss: appId,
     // jti: Unique nonce. Generate a new value per request (no code modifications are necessary, you can use uuid())
     // This is used to prevent replay attacks.
-    jti: uuid(), 
+    jti: uuid(),
     // aud: who the token is intended for. If you issue tokens for multiple destinations with the same private key
     // this, along with aud validation by other resource servers, will mitigate token reuse between multilpe resource servers
     aud: 'https://api.swanbitcoin.com',
     // scopes: What access you are requesting. This should correspond to the API you are calling.
-    scopes: ["write:trades", "read:trade_quotes"] // what permissions are requested
+    scopes
   }
 
   // In this demo, we're reading the private key from the file system (see README.md for how to generate this key)
@@ -27,29 +27,40 @@ const purchaseAmountUsd = process.env.AMOUNT_USD;
   // https://developers.swanbitcoin.com/docs/personal-access/authentication#securing-private-keys
   const privateKey = fs.readFileSync("private.pem")
 
-  const token = jwt.sign(payload, privateKey, {algorithm: 'RS256', expiresIn: '5s'});
+  return jwt.sign(payload, privateKey, {algorithm: 'RS256', expiresIn: '5s'});
+}
 
+const makeRequest = async ({ scopes, url, params}) => {
   // make your API request
-  const url = 'https://api.dev.swanbitcoin.com/integrations/v20220222/trades/execute/market';
-
-  let response;
+  const token = createApiToken({scopes});
+  const authorizationHeader = `Bearer ${token}`;
 
   try {
-    response = await axios.post(url, 
-      { 
-        fundsType: 'balance', 
-        purchaseAmountUsd
-      }, 
-      { headers: {'Authorization': `Bearer ${token}`}}
-    );
+    response = await axios.post(url, params, { headers: {'Authorization': authorizationHeader}});
 
-    console.log(response.data);
+    return response;
   } catch (e) {
     if (e.response) {
-      console.log(e.response.data);
+      return e.reponse
+      console.error(e, e.response.data);
     } else {
-      console.log(e);
+      return null;
+      console.error(e);
     }
   }
+}
 
+(async() => {
+  const marketBuyResponse = await makeRequest({
+    url: 'https://api.dev.swanbitcoin.com/integrations/v20220222/trades/execute/market',
+    params: {
+      fundsType: 'balance',
+      purchaseAmountUsd
+    },
+    scopes: ['write:trades']
+  });
+
+  if (marketBuyResponse) {
+    console.log("Bought", marketBuyResponse.data.btcAmount, "BTC");
+  }
 })();
