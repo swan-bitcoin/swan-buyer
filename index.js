f4d1c717679a0d7cb9b33ff57fd8af75c567cb4e
Yan Pritzker <yan@Yans-MacBook-Pro.local>
2022-06-05 09:46:06 -0400

    Improve comments, fix url and response logging

--- index.js
+++ index.js
@@ -9,21 +9,28 @@ const purchaseAmountUsd = process.env.AMOUNT_USD;
 (async() => {
   // Example payload to make a trading (buy Bitcoin) request
   const payload = {
-    iss: appId, // who you claim to be
-    jti: uuid(), // unique nonce 
-    aud: 'swanbitcoin.com', // who the token is intended for
+    // iss: Who you claim to be. Our server will validate this using the public key you uploaded, and the private key you used to sign this message below
+    iss: appId,
+    // jti: Unique nonce. Generate a new value per request (no code modifications are necessary, you can use uuid())
+    // This is used to prevent replay attacks.
+    jti: uuid(), 
+    // aud: who the token is intended for. If you issue tokens for multiple destinations with the same private key
+    // this, along with aud validation by other resource servers, will mitigate token reuse between multilpe resource servers
+    aud: 'https://api.swanbitcoin.com',
+    // scopes: What access you are requesting. This should correspond to the API you are calling.
     scopes: ["write:trades", "read:trade_quotes"] // what permissions are requested
   }
 
-  // In this demo, we're reading the private key from the file system
-  // and storing it in git.  DO NOT DO THIS in production. Read about securing your key here:
+  // In this demo, we're reading the private key from the file system (see README.md for how to generate this key)
+  //
+  // DO NOT DO THIS in production. Read about securing your key here:
   // https://developers.swanbitcoin.com/docs/personal-access/authentication#securing-private-keys
   const privateKey = fs.readFileSync("private.pem")
 
   const token = jwt.sign(payload, privateKey, {algorithm: 'RS256', expiresIn: '5s'});
 
   // make your API request
-  const url = 'https://dev-api.swanbitcoin.com/integrations/v20220222/trades/execute/market';
+  const url = 'https://api.dev.swanbitcoin.com/integrations/v20220222/trades/execute/market';
 
   let response;
 
@@ -36,7 +43,7 @@ const purchaseAmountUsd = process.env.AMOUNT_USD;
       { headers: {'Authorization': `Bearer ${token}`}}
     );
 
-    console.log(response.body);
+    console.log(response.data);
   } catch (e) {
     if (e.response) {
       console.log(e.response.data);
