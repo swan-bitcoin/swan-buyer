5ff4868e3a3fea24206e19d40e3c227619f3745b
Yan Pritzker <yan@yans-mbp.lan>
2022-06-07 12:59:22 -0500

    Cleanup

--- index.js
+++ index.js
@@ -17,52 +17,51 @@ const key_arn = "arn:aws:kms:us-east-1:165441122072:key/6c295223-543e-4bef-99c6-
 const asn1 = require('asn1.js');
 
 const EcdsaDerSig = asn1.define('ECPrivateKey', function() {
-    return this.seq().obj(
-        this.key('r').int(),
-        this.key('s').int()
-    );
+  return this.seq().obj(
+    this.key('r').int(),
+    this.key('s').int()
+  );
 });
 
 function asn1SigSigToConcatSig(asn1SigBuffer) {
-    const rsSig = EcdsaDerSig.decode(asn1SigBuffer, 'der');
-    return Buffer.concat([
-        rsSig.r.toArrayLike(Buffer, 'be', 32),
-        rsSig.s.toArrayLike(Buffer, 'be', 32)
-    ]);
+  const rsSig = EcdsaDerSig.decode(asn1SigBuffer, 'der');
+  return Buffer.concat([
+    rsSig.r.toArrayLike(Buffer, 'be', 32),
+    rsSig.s.toArrayLike(Buffer, 'be', 32)
+  ]);
 }
 
 
 const kmsSign = async(headers, payload) => {
-      payload.iat = Math.floor(Date.now() / 1000);
-      payload.exp = payload.iat + 5
+  payload.iat = Math.floor(Date.now() / 1000);
+  payload.exp = payload.iat + 5
 
-      let token_components = {
-          header: base64url(JSON.stringify(headers)),
-          payload: base64url(JSON.stringify(payload)),
-      };
+  let token_components = {
+    header: base64url(JSON.stringify(headers)),
+    payload: base64url(JSON.stringify(payload)),
+  };
 
-      let message = Buffer.from(token_components.header + "." + token_components.payload)
+  let message = Buffer.from(token_components.header + "." + token_components.payload)
 
-      let res = await kms.sign({
-          Message: message,
-          KeyId: key_arn,
-          SigningAlgorithm: 'ECDSA_SHA_256',
-          MessageType: 'RAW'
-      }).promise()
+  let res = await kms.sign({
+    Message: message,
+    KeyId: key_arn,
+    SigningAlgorithm: 'ECDSA_SHA_256',
+    MessageType: 'RAW'
+  }).promise()
 
-      let buff = new Buffer(res.Signature.toString('base64'), 'base64');
+  let buff = new Buffer(res.Signature.toString('base64'), 'base64');
 
-      // KMS returns keys in asn1 format. We need to convert them to make the server happy
-      const concatSig = asn1SigSigToConcatSig(buff);
+  // KMS returns keys in asn1 format. We need to convert them to make the server happy
+  const concatSig = asn1SigSigToConcatSig(buff);
 
-      token_components.signature = concatSig.toString("base64")
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=/g, '');
+  token_components.signature = concatSig.toString("base64")
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
 
-      return token_components.header + "." + token_components.payload + "." + token_components.signature;
-
-  }
+  return token_components.header + "." + token_components.payload + "." + token_components.signature;
+}
 
 const createApiToken = async ({ scopes }) => {
   // Example payload to make a trading (buy Bitcoin) request
@@ -88,11 +87,11 @@ const createApiToken = async ({ scopes }) => {
 const makeRequest = async ({ scopes, url, params}) => {
   const token = await createApiToken({scopes});
 
-console.log({token});
-console.log({decoded: jwt.decode(token)});
+  console.log({token});
+  console.log({decoded: jwt.decode(token)});
 
-// Bug right here - KMS is giving us a 71 byte signature instead of 64 bytes
-console.log({verified: jwt.verify(token, `-----BEGIN PUBLIC KEY-----
+  // Bug right here - KMS is giving us a 71 byte signature instead of 64 bytes
+  console.log({verified: jwt.verify(token, `-----BEGIN PUBLIC KEY-----
 MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE924UHsh62Rx6Ddk8/926U+OMMAGxoVs/
 M/zbYMeBb61mlPpceoXEgX9cpaXr9iUawiD4oLXDvHdKU6nEOwP+JQ==
 -----END PUBLIC KEY-----`)});
